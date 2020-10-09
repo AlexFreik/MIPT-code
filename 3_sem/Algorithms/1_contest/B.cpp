9ede3937955a08b428c97008601c30d68bf48fcf
Alex <61039123+Falier77777@users.noreply.github.com>
2020-10-09 05:50:35 +0300

    Create B.cpp

---
+++ 3_sem/Algorithms/1_contest/B.cpp
@@ -0,0 +1,98 @@
+/*
+ B. Палиндромы
+ 
+ Ограничение времени    1 секунда
+ Ограничение памяти    10Mb
+ Ввод    стандартный ввод или input.txt
+ Вывод    стандартный вывод или output.txt
+ Строка называется палиндромом, если она одинаково читается как слева направо,
+ так и справа налево. Например, «abba» – палиндром, а «roman» – нет.
+ Для заданной строки s длины n (1 ≤ n ≤ 105) требуется подсчитать число
+ пар (i, j), 1 ≤ i < j ≤ n, таких что подстрока s[i..j] является палиндромом.
+ Формат ввода
+ 
+ Входной файл содержит одну строку s длины n, состоящюю
+ из маленьких латинских букв.
+ Формат вывода
+ 
+ В выходной файл выведите искомое число подстрок-палиндромов.
+ Пример 1
+ 
+ Ввод    Вывод
+ aaa
+ 3
+ Пример 2
+ 
+ Ввод    Вывод
+ abba
+ 2
+ Пример 3
+ 
+ Ввод    Вывод
+ roman
+ 0
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+template<typename T>
+uint64_t manacher(const std::string &text) {
+    std::vector<T> odd_pal(text.size(), 0), even_pal(text.size(), 0);
+    for (T left = 0, right = 0, cnt = 1; cnt < text.size(); ++cnt) {
+        if (right >= cnt) {
+            odd_pal[cnt] = std::min(right - cnt, odd_pal[left + (right - cnt)]);
+        }
+        while (0 < cnt - odd_pal[cnt] &&
+               cnt + odd_pal[cnt] + 1 < text.size() &&
+               text[cnt - odd_pal[cnt] - 1] == text[cnt + odd_pal[cnt] + 1]) {
+            ++odd_pal[cnt];
+        }
+        if (cnt + odd_pal[cnt] > right) {
+            right = cnt + odd_pal[cnt];
+            left = cnt - odd_pal[cnt];
+        }
+    }
+    for (T left = 0, right = 0, cnt = 1; cnt < text.size(); ++cnt) {
+        if (right >= cnt) {
+            even_pal[cnt] = std::min(right - cnt + 1,
+                                     even_pal[left + (right - cnt) + 1]);
+        }
+        while (0 < cnt - even_pal[cnt]  &&
+               cnt + even_pal[cnt] < text.size() &&
+               text[cnt - even_pal[cnt] - 1] == text[cnt + even_pal[cnt]]) {
+            ++even_pal[cnt];
+        }
+        if (cnt + even_pal[cnt] - 1 > right) {
+            right = cnt + even_pal[cnt] - 1;
+            left = cnt - even_pal[cnt];
+        }
+    }
+    uint64_t ans = 0;
+    for (auto num : odd_pal) {
+        if (num) {
+            ans += num;
+        }
+    }
+    for (auto num : even_pal) {
+        if (num) {
+            ans += num;
+        }
+    }
+    return ans;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // input
+    std::string text;
+    std::cin >> text;
+    assert(text.size() <= 100000);
+    // algorithm
+    uint64_t ans = manacher<int32_t>(text);
+    // output
+    std::cout << ans << std::endl;
+}
