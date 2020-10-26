834cd9fb583dcad76122e4c772ca177ac3a73dff
Alex <61039123+Falier77777@users.noreply.github.com>
2020-10-26 13:12:37 +0300

    Update F.cpp

--- 3_sem/Algorithms/2_contest/F.cpp
+++ 3_sem/Algorithms/2_contest/F.cpp
@@ -1,28 +1,25 @@
 /*
-https://contest.yandex.ru/contest/20019/run-report/37021819/
-F. K-я общая подстрока
+ https://contest.yandex.ru/contest/20019/run-report/38160942/
+ F. K-я общая подстрока
+ 
+ Ограничение времени    1 секунда
+ Ограничение памяти    256Mb
+ Ввод    стандартный ввод или m5.in
+ Вывод    стандартный вывод или m5.out
+ Заданы две строки s, t и целое число k. Рассмотрим множество всех таких непустых строк, которые встречаются как подстроки в s и t одновременно. Найдите k-ую в лексикографическом порядке строку из этого множества.
+ Формат ввода
 
-Ограничение времени	1 секунда
-Ограничение памяти	256Mb
-Ввод	стандартный ввод или m5.in
-Вывод	стандартный вывод или m5.out
-Заданы две строки s, t и целое число k. Рассмотрим множество всех таких непустых строк, 
-которые встречаются как подстроки в s и t одновременно.
-Найдите k-ую в лексикографическом порядке строку из этого множества.
-Формат ввода
+ В первых двух строках записаны строки s и t (1 ≤ |s|, |t| ≤ 105). В третьей строке записано целое число k (1 ≤ k ≤ 1018). Строки состоят из маленьких латинских букв.
+ Формат вывода
 
-В первых двух строках записаны строки s и t (1 ≤ |s|, |t| ≤ 105). 
-В третьей строке записано целое число k (1 ≤ k ≤ 1018). Строки состоят из маленьких латинских букв.
-Формат вывода
+ В первой строке выведите искомую строку или -1, если такой не существует.
+ Пример
 
-В первой строке выведите искомую строку или -1, если такой не существует.
-Пример
-
-Ввод	Вывод
-aaa
-abaa
-3
--1
+ Ввод    Вывод
+ aaa
+ abaa
+ 3
+ -1
 */
 
 #include <iostream>
@@ -31,59 +28,31 @@ abaa
 #include <algorithm>
 #include <cassert>
 #include <map>
+#include <optional>
 
 namespace suff_array {
 // begin of the namespace
 template<typename T>
 class suff_array {
-    struct ring {
-        explicit ring(size_t base) : base(base) {}
-        size_t base;
-        T sub(T value, T subb) const;
-        T add(T value, T add) const;
-    };
-    void build_suff_array_first_step();
-    void build_suff_array_next_step(size_t k);
+    void build_suff_array_preparing();
+    void build_suff_array_step(size_t k);
     std::vector<T> eq_class;
     T eq_class_number;
-    ring R;
     bool from_diff_str(T ind1, T ind2, T first_str_size);
 
   public:
     explicit suff_array(std::string &&text);
     void kasai();
-    std::string get_substring(uint64_t k, T first_str_size);
+    size_t substring_number();
     void print_array(bool lcp = false) const;
+    std::string get_substring(uint64_t k, T first_str_size);
     std::string text;
     std::vector<T> array;
     std::vector<T> lcp;
 };
 
 template<typename T>
-T suff_array<T>::ring::sub(T value, T subb) const {
-    assert(value < base && subb < base);
-    if (value >= subb) {
-        value -= subb;
-    } else {
-        value +=  base - subb;
-    }
-    value = value - static_cast<T>((value / base) * base);
-    if (value < 0) {
-        value += base;
-    }
-    return value;
-}
-template<typename T>
-T suff_array<T>::ring::add(T value, T add) const {
-    assert(add < base && value < base);
-    value += add;
-    if (value > base) {
-        value -= base;
-    }
-    return value;
-}
-template<typename T>
-void suff_array<T>::build_suff_array_first_step() {
+void suff_array<T>::build_suff_array_preparing() {
     assert(array.size() == text.size() + 1);
     assert(eq_class.size() == text.size() + 1);
     std::map<char, std::vector<T>> pockets;
@@ -104,11 +73,14 @@ void suff_array<T>::build_suff_array_first_step() {
     }
 }
 template<typename T>
-void suff_array<T>::build_suff_array_next_step(size_t k) {
-    // create pockets and put suff-s there according to the first part eq_class
+void suff_array<T>::build_suff_array_step(size_t k) {
+    assert(k > 0);
+    // creates pockets and put suff-s there according to the first part eq_class
     std::vector<std::vector<T>> pockets(eq_class_number);
+    T base = static_cast<T>(array.size());
     for (size_t ind = 0; ind < array.size(); ++ind) {
-        T array_ind = R.sub(array[ind], 1 << (k-1));
+        assert(array[ind] < base && (1 << (k - 1)) < base);
+        T array_ind = (array[ind] + base - (1 << (k - 1))) % base;
         pockets[eq_class[array_ind]].push_back(array_ind);
     }
     std::vector<T> temp_eq_classes(eq_class.size());
@@ -121,8 +93,11 @@ void suff_array<T>::build_suff_array_next_step(size_t k) {
         ++ind_cnt;
         for (T ind = 1; ind < pocket.size(); ++ind) {
             array[ind_cnt] = pocket[ind];
-            T curr_second_part_class = eq_class[R.add(pocket[ind], 1 << (k-1))],
-            prev_second_part_class = eq_class[R.add(pocket[ind - 1], 1 << (k-1))];
+            assert(pocket[ind] < base && (1 << (k - 1)) < base);
+            T second_part_ind = (pocket[ind] + (1 << (k - 1))) % base;
+            T prev_2nd_part_ind = (pocket[ind - 1] + (1 << (k - 1))) % base;
+            T curr_second_part_class = eq_class[second_part_ind];
+            T prev_second_part_class = eq_class[prev_2nd_part_ind];
             assert(curr_second_part_class >= prev_second_part_class);
             if (curr_second_part_class != prev_second_part_class) {
                 ++eq_class_number;
@@ -132,17 +107,17 @@ void suff_array<T>::build_suff_array_next_step(size_t k) {
         }
         ++eq_class_number;
     }
-    eq_class = temp_eq_classes;
+    eq_class = std::move(temp_eq_classes);
 }
 template<typename T>
 suff_array<T>::suff_array(std::string &&text)
-                        : text(text),
-                        array(std::vector<T>(text.size() + 1)),
-                        eq_class(std::vector<T>(text.size() + 1)),
-                        R(text.size() + 1) {
-    build_suff_array_first_step();
-    for (size_t k = 0; 1 << k < array.size(); ++k) {
-        build_suff_array_next_step(k + 1);
+                        : eq_class(std::vector<T>(text.size() + 1)),
+                        text(text),
+                        array(std::vector<T>(text.size() + 1)) {
+    build_suff_array_preparing();
+    for (size_t k = 1; 1 << (k - 1) < array.size(); ++k) {
+        // on each step work with
+        build_suff_array_step(k);
         if (eq_class_number == eq_class.size()) {
             break;
         }
@@ -162,57 +137,33 @@ void suff_array<T>::print_array(bool lcp) const {
     }
 }
 template<typename T>
-T diff(T a, T b) {
-    if (a >= b) return a - b;
-    return b - a;
-}
-template<typename T>
 void suff_array<T>::kasai() {
     lcp.clear();
     if (array.size() == 1) { return; }
     lcp = std::vector<T>(array.size());
     // inv_arr[i] -- number of i suff in arr
-    std::vector<T> inverse_array(array.size());
-    for (T ind = 0; ind < array.size(); ++ind) {
-        inverse_array[array[ind]] = ind;
-    }
+    const std::vector<T> &inverse_array = eq_class;
     assert(array[0] == array.size() - 1);
-    T cnt = inverse_array[0];
-    for (size_t ind2 = array[cnt - 1]; ind2 < text.size(); ++ind2) {
-        if (text[ind2] == text[ind2 - array[cnt - 1] + 0]) {
-            ++lcp[cnt];
-        } else {
-            break;
-        }
-    }
-    for (T ind = 1; ind < array.size() - 1; ++ind) {
-        T cnt = inverse_array[ind], prev_lcp = lcp[inverse_array[ind - 1]];
-        lcp[cnt] = (prev_lcp > 1 ? prev_lcp - 1 : 0);
-        T dif = diff(ind, array[cnt - 1]), min = std::min(array[cnt - 1], ind);
-        for (size_t ind2 = min + dif + lcp[cnt]; ind2 < text.size(); ++ind2) {
-            if (text[ind2] == text[ind2 - dif]) {
-                ++lcp[cnt];
-            } else {
-                break;
-            }
-        }
+    for (T ind = 0, cnt = inverse_array[0], prev_lcp = 1;
+         ind < array.size() - 1;
+         prev_lcp = std::max<T>(lcp[cnt], 1), ++ind, cnt = inverse_array[ind]) {
+        lcp[cnt] = prev_lcp - 1;
+        T prev_suff = array[cnt - 1];
+        T max = std::max(prev_suff, ind);
+        while (max + lcp[cnt] < text.size() &&
+               text[ind + lcp[cnt]] == text[prev_suff + lcp[cnt]]) { ++lcp[cnt]; }
     }
 }
-
 template<typename T>
 bool suff_array<T>::from_diff_str(T ind1, T ind2, T first_str_size) {
-    bool ret_val = true;
-    if (ind1 >= first_str_size && ind2 >= first_str_size) {
-        ret_val = false;
-    } else if (ind1 < first_str_size && ind2 < first_str_size) {
-        ret_val = false;
-    }
-    return ret_val;
+    return !((ind1 >= first_str_size && ind2 >= first_str_size) ||
+            (ind1 <  first_str_size && ind2 <  first_str_size));
 }
 template<typename T>
 std::string suff_array<T>::get_substring(uint64_t k, T first_str_size) {
+    assert(k != 0);
     kasai();
-    std::string ret_str;
+    std::string ans_str;
     for (T ind = 1, prev_lcp = 0; ind < lcp.size(); ++ind) {
         prev_lcp = std::min(prev_lcp, lcp[ind]);
         T diff = lcp[ind] - prev_lcp;
@@ -222,24 +173,23 @@ std::string suff_array<T>::get_substring(uint64_t k, T first_str_size) {
                 prev_lcp = prev_lcp + diff;
             } else {
                 assert(k > 0);
-                ret_str = std::string(text, array[ind], prev_lcp + k);
+                ans_str = std::string(text, array[ind], prev_lcp + k);
                 k = 0;
                 break;
             }
         }
     }
-    assert(!(ret_str.size() == 0) || k > 0);
-    return ret_str;
+    assert(!ans_str.empty() || k > 0);
+    return ans_str;
 }
 }  // namespace suff_array
 
-std::string funk(std::string &str1, std::string &str2, uint64_t k) {
-    uint32_t str1_size = static_cast<uint32_t>(str1.size());
+std::string ans_calculation(const std::string &str1,
+                                           const std::string &str2, uint64_t k) {
     char min_sympol = 0;
-    (str1 += min_sympol) += str2;
-    std::string ret_str =
-    suff_array::suff_array<uint32_t>(std::move(str1)).get_substring(k, str1_size);
-    return ret_str;
+    std::string text = str1 + min_sympol + str2;
+    return suff_array::suff_array<uint32_t>(std::move(text)).
+    get_substring(k, static_cast<uint32_t>(str1.size()));
 }
 
 int main(int argc, const char * argv[]) {
@@ -248,9 +198,9 @@ int main(int argc, const char * argv[]) {
     uint64_t k;
     std::cin >> str1 >> str2 >> k;
     // funk
-    std::string ans = funk(str1, str2, k);
+    std::string ans = ans_calculation(str1, str2, k);
     // print answer
-    if (ans.size()) {
+    if (!ans.empty()) {
         std::cout << ans << std::endl;
     } else {
         std::cout << -1 << std::endl;
